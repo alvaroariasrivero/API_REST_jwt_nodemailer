e7cd1f7331dd6f0f0eca66e378072c0141fbe115
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-03-07 12:48:17 +0100

    user apit routes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const usersApi = require('./controllers/usersapi')
 
 require('./utils/dbmongo');
 
 app.get('/', (req, res) => {
     res.send('Mierda mundo')
 });
+
+app.get('/api/users', usersApi.getUser)
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- controllers/usersapi.js
+++ controllers/usersapi.js
@@ -1,1 +1,17 @@
-const User = require('../models/user')
+const User = require('../models/user');
+
+const getUser = async(req, res) => {
+    let data;
+    try {
+        data = await User.find({'username': req.body.username, 'password': req.body.password}, )
+        res.status(200).json(data);
+    } catch (error) {
+        console.log('Error:', error)
+    }
+}
+
+const user = {
+    getUser
+}
+
+module.exports = user
