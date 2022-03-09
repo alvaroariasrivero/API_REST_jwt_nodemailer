8d01f9da52f351852330ef60b12cf66ee19e0a38
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-03-09 14:06:46 +0100

    uninstall cors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
 require('./utils/dbmongo');
 
@@ -10,7 +9,6 @@ const errors = require('./middlewares/errors');
 const app = express();
 const port = 3000;
 
-app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
