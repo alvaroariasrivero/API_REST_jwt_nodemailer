43343a2f98581182888f16bcc90f9cee51c5278b
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-03-10 14:11:38 +0100

    views created

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ const port = 3000;
 
 app.use(express.json());
 app.set('view engine', 'pug');
+app.set('views','./views');
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.render('home')
+});
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+app.get('/signup', (req, res) => {
+    res.render('signup')
+});
+app.get('/products', (req, res) => {
+    res.render('products')
 });
 
 //Rutas API
